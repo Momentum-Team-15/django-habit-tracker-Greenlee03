c6479750655d27ca639836e5ee825133222cfadd
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-11-03 09:41:33 -0400

    allowed hosts update

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = [dpg-cdhs16qrrk06v9shvq1g-a]
+ALLOWED_HOSTS = []
 
 if env("RENDER"):
     ALLOWED_HOSTS.append(env("RENDER_EXTERNAL_HOSTNAME"))
