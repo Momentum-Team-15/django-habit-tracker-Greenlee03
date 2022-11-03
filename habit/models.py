c8c4edd7808bb40549fe22cb308af6e0e3317efc
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-11-03 05:25:57 -0400

    User update

--- habit/models.py
+++ habit/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class User(models.Model):
+class User(AbstractUser):
     pass
 
 class Habit(models.Model):
